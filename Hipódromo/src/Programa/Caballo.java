9378f8b7a53b7ee8725a39afc4595f16fd539ff9
AndyArrieta <andrestransformers@hotmail.com>
2022-06-12 19:33:59 -0500

    1ER AVANCE+apuestas

--- Hipódromo/src/Programa/Caballo.java
+++ Hipódromo/src/Programa/Caballo.java
@@ -9,5 +9,48 @@ package Programa;
  * @author acer
  */
 public class Caballo {
+    String nombre_cab;
+    Double peso;
+    int edad;
+    String raza;
+
+    public Caballo(String nombre_cab, Double peso, int edad, String raza) {
+        this.nombre_cab = nombre_cab;
+        this.peso = peso;
+        this.edad = edad;
+        this.raza = raza;
+    }
+
+    public String getNombre_cab() {
+        return nombre_cab;
+    }
+
+    public void setNombre_cab(String nombre_cab) {
+        this.nombre_cab = nombre_cab;
+    }
+
+    public Double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(Double peso) {
+        this.peso = peso;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
     
 }
--- Hipódromo/src/Programa/Carrera.java
+++ Hipódromo/src/Programa/Carrera.java
@@ -4,10 +4,40 @@
  */
 package Programa;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author acer
  */
 public class Carrera {
+    int numero_car;
+    String nombre_jock;
+    String aux_nombreCab;
+    Double distancia;
+    Double tiempo_reg;
+    
+    Caballo cab;
+    ArrayList<Caballo>listcaballos;
+    
+    public void regCaballo(String nombre_cab, Double peso, int edad, String raza){
+        cab=new Caballo(nombre_cab, peso, edad, raza);
+        listcaballos.add(cab);
+    }
+    public void regJockey(){
+        
+    }
+    
+    public Carrera(String nombre_jock, String aux_nombreCab, Double distancia, Double tiempo_reg) {
+        for (int i=0;i<=listcaballos.size();i++) {
+            if (listcaballos.get(i).getNombre_cab()==aux_nombreCab) {
+                this.nombre_jock = nombre_jock;
+                this.aux_nombreCab = aux_nombreCab;
+                this.distancia = distancia;
+                this.tiempo_reg = tiempo_reg;
+            }
+        }
+    }
     
 }
+
